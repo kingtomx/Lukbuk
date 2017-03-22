760b250d9042118f5a81a13a2c9512a38eb63e74
tomas <tomas.pascual.home@gmail.com>
2017-03-22 16:27:24 -0300

    commit

---
+++ Lukbuk/LukbukImage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Lukbuk
+{
+	public partial class LukbukImage : ContentView
+	{
+		public LukbukImage()
+		{
+			InitializeComponent();
+
+		}
+	}
+}
---
+++ Lukbuk/LukbukPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace Lukbuk
+{
+	public partial class LukbukPage : ContentPage
+	{
+		public LukbukPage()
+		{
+			InitializeComponent();
+		}
+
+
+		protected override void OnAppearing()
+		{
+			App.Current.MainPage = new NavigationPage();
+			App.Current.MainPage.Navigation.PushAsync(new LukbukPageList());
+		}
+
+	}
+}
